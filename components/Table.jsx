d9df1d404f27d49926e55cd4d344e5e0fc860341
ijaz khan <88775028+IjazkhanAfridi@users.noreply.github.com>
2023-04-15 02:26:14 +0500

    search updated

--- components/Table.jsx
+++ components/Table.jsx
@@ -21,6 +21,7 @@ const Table = () => {
   const [groupDropdown, setGroupDropdown] = useState(false);
   const [grouping, setGrouping] = useState();
   const [groupDatashow, setGroupDatashow] = useState(false);
+  const [searchTerm, setSearchTerm] = useState('');
 
   // const [tableData, setTableData] = useState(TableData);
   console.log('table data :', filteredData);
@@ -57,13 +58,20 @@ const Table = () => {
   };
 
   const handleSearch = (e) => {
-    let val = e.target.value;
+    setSearchTerm(e.target.value);
     const temp = [...TableData];
     setFilteredData(
+      // temp.filter((item) => {
+      //   if (item.status.includes(val)) {
+      //     return item.status.includes(val);
+      //   }
+      // })
       temp.filter((item) => {
-        if (item.status.includes(val)) {
-          return item.status.includes(val);
-        }
+        // return true if the item matches the search term
+        return Object.values(item)
+          .join(' ')
+          .toLowerCase()
+          .includes(searchTerm.toLowerCase());
       })
     );
   };
@@ -141,7 +149,6 @@ const Table = () => {
 
   return (
     <div className='w-full'>
-
       <iframe
         id='ifmcontentstoprint'
         style={{ height: '0px', width: '0px', position: 'absolute' }}
@@ -210,7 +217,7 @@ const Table = () => {
             <input
               type='text'
               id='table-search-users'
-              // value={name}
+              value={searchTerm}
               className='block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
               placeholder='Search...'
               onChange={handleSearch}
@@ -392,7 +399,7 @@ const Table = () => {
                         Data Grouped By : {grouping}
                       </th>
                     </tr>
-                    {groupItems.map((item) => (
+                    {groupItems.map((item, i) => (
                       <>
                         {/* <tr
                     key={item.id}
@@ -408,7 +415,7 @@ const Table = () => {
                     </th>
                   </tr> */}
                         <tr
-                          key={item.id}
+                          key={i}
                           className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'
                         >
                           <th
@@ -439,21 +446,3 @@ const Table = () => {
 };
 
 export default Table;
-
-const SortArrow = () => {
-  return (
-    <>
-      <span>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='w-3 h-3 ml-1'
-          aria-hidden='true'
-          fill='currentColor'
-          viewBox='0 0 320 512'
-        >
-          <path d='M27.66 224h264.7c24.6 0 36.89-29.78 19.54-47.12l-132.3-136.8c-5.406-5.406-12.47-8.107-19.53-8.107c-7.055 0-14.09 2.701-19.45 8.107L8.119 176.9C-9.229 194.2 3.055 224 27.66 224zM292.3 288H27.66c-24.6 0-36.89 29.77-19.54 47.12l132.5 136.8C145.9 477.3 152.1 480 160 480c7.053 0 14.12-2.703 19.53-8.109l132.3-136.8C329.2 317.8 316.9 288 292.3 288z' />
-        </svg>
-      </span>
-    </>
-  );
-};
